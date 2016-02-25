6c927f29e2251ea49eedd7695c84d5561dc996ca
franciscosucre <frank91frank@gmail.com>
2016-02-25 14:24:31 -0430

    (FJSG) Inicializacion del repositorio

---
+++ ConectarseAlLdc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# ssh <username>@<host>
+ssh 10-10717@sheeana.ldc.usb.ve
+
---
+++ limpiarYEnviarAlLdc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make clean
+# scp [-r] <file(s)> <username>@<host>:<destinyDirectory>
+scp -r ./* 10-10717@sheeana.ldc.usb.ve:/preg/10-10717/UsoDisco_Operativos_1
+
